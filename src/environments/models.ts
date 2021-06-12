20c7c10e1a2bf85a7bae51e6ee00336e48e5e48c
Sousa <1080184@isep.ipp.pt>
2021-06-12 14:18:16 +0100

    chore: remove unused file

--- src/environments/models.ts
+++
@@ -1,7 +0,0 @@
-import { INestApplication } from '@nestjs/common';
-import { OpenAPIObject } from '@nestjs/swagger';
-
-export interface Environment {
-  production: boolean;
-  swaggerInitializer: (app: INestApplication) => OpenAPIObject;
-}
